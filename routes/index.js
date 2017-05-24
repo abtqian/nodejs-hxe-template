e04bdc6069586cff3d0b92aef7a5f9d9ea58d1e2
Albert Qian <i327608@mo-7bcd9292b.mo.sap.corp>
2017-05-24 08:42:20 +0000

    project

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var hdb    = require('hdb');
+ 
+var router = express.Router();
+var client = hdb.createClient({
+  host     : '35.185.237.5',
+  port     : 39013,
+  user     : 'SYSTEM',
+  password : 'bemT4amt'
+});
+ 
+var sql = "select * from m_database";
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  client.connect(function (err) {
+  if (err) {
+          return console.error('Connect error', err);
+  }
+ 
+   console.log("Connected");
+   client.exec(sql, function(err, rows) {
+      res.render('index', { title: 'GCP app (Node.js) for HXE Demo', datarows: rows });
+    });
+    client.end();
+    if (err) {
+      return console.error('Execute error:', err);
+    }
+  });
+});
+ 
+module.exports = router;
+
